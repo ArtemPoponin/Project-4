1d7b4ac89b2d2d3faebdd30aac5bdfd6e4c0cb57
Artem Poponin <artempoponin1@gmail.com>
2021-04-11 22:25:08 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean registered = true;
+        long price = 1459;
+        long bonus = price/20;
+        System.out.println(bonus);
+    }
+}
